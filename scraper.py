432b4dcbe0663af1f376e8c78a2ed8c73166ce36
mariama1ieva <143318148+mariama1ieva@users.noreply.github.com>
2026-03-12 01:43:06 +0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,36 +12,42 @@ headers = {
 }
 
 def send_photo(photo, caption):
+
     api = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
+
     requests.post(api, data={
         "chat_id": CHAT_ID,
         "photo": photo,
         "caption": caption
     })
 
-print("Loading page...")
+
+print("Loading bina.az")
 
 r = requests.get(URL, headers=headers)
-print("Status:", r.status_code)
 
 soup = BeautifulSoup(r.text, "html.parser")
 
 cards = soup.select('[data-cy="item-card"]')
+
 print("Found:", len(cards))
 
 for card in cards[:10]:
+
     link_tag = card.select_one('[data-cy="item-card-link"]')
     price_tag = card.select_one('[data-cy="item-card-price-full"]')
+    location_tag = card.select_one('[data-cy="city_when"]')
     img_tag = card.select_one("img")
 
     if not link_tag:
         continue
 
-    link = "https://bina.az" + link_tag["href"]
+    link = link_tag["href"]
     price = price_tag.text.strip() if price_tag else ""
+    location = location_tag.text.strip() if location_tag else ""
     image = img_tag["src"] if img_tag else ""
 
-    text = f"{price}\n{link}"
+    text = f"{price} AZN\n{location}\n{link}"
 
     print("Sending:", link)
 
