df7dae0a3ce8f848b5857d213ab51776ea296fc9
mariama1ieva <143318148+mariama1ieva@users.noreply.github.com>
2026-03-19 00:00:42 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+TELEGRAM_TOKEN = "YOUR_TOKEN"
+CHAT_ID = "YOUR_CHAT_ID"
+
+BASE_URL = "https://bina.az/alqi-satqi/menziller"
+
+FILTER_URL = BASE_URL + "?has_cup=true"
+
+CHECK_INTERVAL = 600  # 10 dəqiqə
